ac989cbce3c197e441cb272ff27601f747412a1e
Jake Halloran <jakehalloran2@gmail.com>
2017-04-26 23:12:02 -0400

    Update for submission

--- Part2/submission/static/php/spark-search.php
+++ Part2/submission/static/php/spark-search.php
@@ -7,9 +7,9 @@
  // $deleteString("hdfs dfs -rmr /web1search");
   //echo(exec($deleteString));
   $bash1 = 'bash ../scripts/hdfs_clear.sh';
-  $bash2 = 'bash ../scripts/generate_results.sh "'.$searchTerms.'"';
+  $bash2 = 'bash ../scripts/generate_results_spark.sh "'.$searchTerms.'"';
   $bash3= 'bash ../scripts/json_clear.sh';
-  $bash4 = 'bash ../scripts/json_move.sh';
+  $bash4 = 'bash ../scripts/json_move_spark.sh';
   echo($bash2);
   exec($bash1);
   exec($bash2);
